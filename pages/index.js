443bc04920017a0f0613d35468ee26de1e161704
kelvin tony <kelvinudemy@gmail.com>
2022-12-10 01:24:51 +0100

    changes aa

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,24 @@ import styles from '../styles/Home.module.css';
 import Navbar from '../components/Navbar/Navbar';
 import comingsoon from '../assets/comingsoon.png';
 
-export default function Home() {
+export default function Home({cookies}) {
 	return (
 		<div>
 			<Navbar />
 			<div className={styles.home_container}>
 				<h1>Home</h1>
+				{cookies?.access_token && <p>cookie is present live</p>}
 				<Image className={styles.home_image} src={comingsoon} alt='pix-a' />
 			</div>
 		</div>
 	);
 }
+
+export const getServerSideProps = async (context) => {
+	const cookies = context?.req?.cookies;
+	// console.log(cookies);
+
+	return {
+		props: { cookies }
+	};
+};
