55fc95ebf5708e3f98e786e0520a502305f6544f
kelvin tony <kelvinudemy@gmail.com>
2023-02-04 00:06:27 +0100

    initial 11

---
+++ components/Loader/Loader.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './Loader.module.css';
+
+const Loader = () => {
+  return (
+    <div className={styles.lds_default}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Loader;
