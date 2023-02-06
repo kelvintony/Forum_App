53c67ed4cf68d259c3c611a7b659a0e864686da4
kelvin tony <kelvinudemy@gmail.com>
2023-02-06 18:07:01 +0100

    initial 6

--- pages/post/community-post/[id].js
+++ pages/post/community-post/[id].js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect, useReducer } from 'react';
 import Loader from '../../../components/Loader/Loader';
 
-import db from '../../../utils/db';
 import postModel from '../../../models/post';
+import db from '../../../utils/db';
 
 import styles2 from '../../../sections/home/MainSection.module.css';
 
