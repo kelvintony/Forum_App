b5da3e3c5dde45484477a135f173b82ac33cc84c
kelvin tony <kelvinudemy@gmail.com>
2023-02-06 17:57:03 +0100

    initial 5

--- models/user.js
+++ models/user.js
@@ -1,18 +1,18 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema(
-	{
-		username: { type: String, required: true },
-		email: { type: String, required: true, unique: true },
-		password: { type: String, required: true },
-		isAdmin: { type: Boolean, required: true, default: false },
-		verified: { type: Boolean, default: false }
-	},
-	{
-		timestamps: true
-	}
+  {
+    username: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, required: true, default: false },
+    verified: { type: Boolean, default: false },
+  },
+  {
+    timestamps: true,
+  }
 );
 
-const user = mongoose.models.user || mongoose.model('user', userSchema);
+// const user = mongoose.models.user || mongoose.model('user', userSchema);
 
-export default user;
+export default mongoose.models.user || mongoose.model('user', userSchema);
