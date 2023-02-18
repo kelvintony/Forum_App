cab77935c1911ca047eebb9fc73854c7710842d6
kelvin tony <kelvinudemy@gmail.com>
2023-02-18 05:07:46 +0100

    initial 1

---
+++ pages/api/cloudinary-sign.js
@@ -0,0 +1,11 @@
+import cloudinaryModule from 'cloudinary';
+
+const cloudinary = cloudinaryModule.v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
+});
+
+export default cloudinary;
